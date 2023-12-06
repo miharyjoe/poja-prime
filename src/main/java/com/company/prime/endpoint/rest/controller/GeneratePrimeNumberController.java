048727c15f0431bd67e5cebd8b64ee7dce8c01c7
miharyjoel <miharyjoel@gmail.com>
2023-12-06 11:31:25 +0300

    feat: generate prime number

---
+++ src/main/java/com/company/prime/endpoint/rest/controller/GeneratePrimeNumberController.java
@@ -0,0 +1,19 @@
+package com.company.prime.endpoint.rest.controller;
+
+import com.company.prime.service.event.PrimeNumberService;
+import java.math.BigInteger;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class GeneratePrimeNumberController {
+  private final PrimeNumberService primeNumberService;
+
+  @GetMapping("/new-prime")
+  public BigInteger generateNewPrime() {
+    BigInteger prime = primeNumberService.generatePrimeNumber();
+    return prime;
+  }
+}
